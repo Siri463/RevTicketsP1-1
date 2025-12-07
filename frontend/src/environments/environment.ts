b386b57f469a22e6781332f66bac35dc2d32148f
shashank deva <108784369+Shashankdeva9@users.noreply.github.com>
2025-12-07 17:26:07 +0530

    fix

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8081/api',
+  apiUrl: 'http://13.127.100.123:8080/api',
   stripePublicKey: 'pk_test_your_stripe_public_key_here',
-  wsUrl: 'http://localhost:8081/ws'
+  wsUrl: 'http://13.127.100.123:8080/ws'
 };
